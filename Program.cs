19d4a5e19aa854d6cd1c7c02a1e5d83353ab1ea0
Matthias Feistel <feistel.matthias@gmx.de>
2014-05-29 17:55:53 +0200

    * initial commit * added debugging log4net class

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace SkeggFallLevelDesigner
+{
+     class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod
+().DeclaringType);
+
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+          [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            log.Debug("Application Starting");
+
+        }
+    }
+}
